ba9d7649d41d71928a2c7fd9b8bb45e45c7f9dfa
Eugenia Galantseva <lucyatsky@gmail.com>
2020-10-19 16:17:00 -0500

    added hamburger menu

---
+++ togglenav.js
@@ -0,0 +1,19 @@
+const navbar = document.querySelector(".header-nav");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", toggleHamburger);
+
+function toggleHamburger(){
+    console.log('im trying');
+    navbar.classList.toggle("showNav")
+    hamburger.classList.toggle("showClose")
+  };
+
+const menuItems = document.querySelectorAll(".menu_item");
+
+menuItems.forEach( 
+    function(menuItem) { 
+      menuItem.addEventListener("click", toggleHamburger) 
+    }
+  );
+
